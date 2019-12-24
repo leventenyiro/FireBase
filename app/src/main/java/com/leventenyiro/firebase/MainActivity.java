f2b234c05608eab697045f4c7d457a8ef690cc01
Leviii99 <nyiro.levente@gmail.com>
2019-12-24 10:47:06 +0100

    Check internet connection

--- app/src/main/java/com/leventenyiro/firebase/MainActivity.java
+++ app/src/main/java/com/leventenyiro/firebase/MainActivity.java
@@ -1,15 +1,22 @@
 package com.leventenyiro.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnInsert;
     private DatabaseReference ref;
     private Member member;
+    long id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +34,38 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    id = (dataSnapshot.getChildrenCount());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                member.setName(inputName.getText().toString());
-                member.setEmail(inputEmail.getText().toString());
-                ref.push().setValue(member);
-                Toast.makeText(MainActivity.this, "Sikeres rögzítés", Toast.LENGTH_SHORT).show();
+                if (isNetworkConnected())
+                {
+                    member = new Member(inputName.getText().toString(), inputEmail.getText().toString());
+                    //ref.push().setValue(member);
+                    //ref.child("member1").setValue(member);
+                    Toast.makeText(MainActivity.this, "Sikeres rögzítés", Toast.LENGTH_SHORT).show();
+                    inputName.setText("");
+                    inputEmail.setText("");
+                    ref.child(String.valueOf(id + 1)).setValue(member);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Nincs kapcsolódva az internethez!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -41,7 +74,12 @@ public class MainActivity extends AppCompatActivity {
         inputName = findViewById(R.id.inputName);
         inputEmail = findViewById(R.id.inputEmail);
         btnInsert = findViewById(R.id.btnInsert);
-        member = new Member();
         ref = FirebaseDatabase.getInstance().getReference().child("Member");
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 }
--- app/src/main/java/com/leventenyiro/firebase/Member.java
+++ app/src/main/java/com/leventenyiro/firebase/Member.java
@@ -5,22 +5,16 @@ public class Member
     private String name;
     private String email;
 
-    public Member() {
+    public Member(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
